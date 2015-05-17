5577c91359813ca64f22e26555d1b76c179562f3
P. Thiện <thien92dn@gmail.com>
2015-05-17 09:00:40 +0700

    Add Project

---
+++ FirstProgram/FirstProgram.Shared/StaticMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using  Windows.UI.Popups;
+namespace FirstProgram
+{
+  public  class StaticMethod
+    {
+      public static void ShowMsg(string name)
+      {
+          string text = "Hello " + name;
+#if WINDOWS_APP
+          text += " (Windows)";
+#else
+          text += " (Windows Phone)";
+#endif
+          var msgbox = new MessageDialog(text, "My first program");
+           msgbox.ShowAsync();
+      }
+    }
+}

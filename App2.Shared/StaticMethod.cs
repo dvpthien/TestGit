38fa10889e6731cb23a4f5dffa02d21fad6e5936
P. Thiện <thien92dn@gmail.com>
2015-05-17 08:55:22 +0700

    commit project

---
+++ App2.Shared/StaticMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Popups;
+
+namespace App2
+{
+  public  class StaticMethod
+    {
+      public static void Click(string text)
+      {
+          var dialog = new MessageDialog(text);
+          dialog.ShowAsync();
+      }
+    }
+}

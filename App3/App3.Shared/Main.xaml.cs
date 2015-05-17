fb7d971c76909e85ee5c455a7f63676b2bc0c87e
P. Thiện <thien92dn@gmail.com>
2015-05-17 08:49:57 +0700

    Commit project

---
+++ App3/App3.Shared/Main.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App3
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Main : Page
+    {
+        public Main()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Frame.GoBack();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            string text = e.Parameter.ToString();
+            text = "abc";
+            Frame.GoBack();
+           
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            (new MessageDialog(txtName.Text + " abcd","Day la tieu de")).ShowAsync();
+        }
+    }
+}
